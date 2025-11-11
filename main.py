400bed6fbf0d8465f583778f4d6dc7fe5f74bfe3
baxrom-create <baxrom008iz@gmail.com>
2025-11-11 16:19:19 +0500

    yangi narsalar qowildi

---
+++ main.py
@@ -0,0 +1,37 @@
+# Задания
+# Создать программу которая спрашивает от пользователя название страны
+# И выдаёт информацию о стране
+# Решения
+import requests
+
+while True:
+    country = input("Введите страну: ")
+    if not country:
+        break
+
+    url = f"https://restcountries.com/v3.1/name/{country}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("Ошибка запроса или страна не найдена.")
+        continue
+
+    data = response.json()
+    if isinstance(data, list):
+        info = data[0]
+
+        name = info["name"]["common"]
+        capital = info.get("capital", ["Нет данных"])[0]
+        currencies = list(info.get("currencies", {}).keys())
+        languages = list(info.get("languages", {}).values())
+        population = info.get("population", "Нет данных")
+        area = info.get("area", "Нет данных")
+
+        print(f"Страна: {name}")
+        print(f"Столица: {capital}")
+        print(f"Валюты: {currencies}")
+        print(f"Языки: {languages}")
+        print(f"Численность населения: {population}")
+        print(f"Площадь: {area}")
+    else:
+        print("Страна не найдена.")
